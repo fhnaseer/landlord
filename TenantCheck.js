a45dba1e1af545393f8756ddf68948dd49ed3d3f
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2015-02-21 19:20:45 +0500

    Added search page

--- TenantCheck.js
+++ TenantCheck.js
@@ -1,19 +1,58 @@
+User = new Mongo.Collection("user");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Session.setDefault('showResult', 0);
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.body.helpers({
+        "response": {
+            "personal": {
+                "name": "Asim Tenny",
+                "dateOfBirth": "1978-12-30T00:00:00.000Z",
+                "cnic": 3740506159771,
+                "currentAddress": "173 Street 34, F-10/1",
+                "permanentAddress": "56 Nust, H-12/2"
+            },
+            "basic": {
+                "riskIndex": 90
+            },
+            "evictions": [
+                {
+                    "case": 234535,
+                    "filing": "2005-11-26T00:00:00.000Z",
+                    "judge": "Abdul Rub Khan",
+                    "order": "evict house immediately and pay 10000"
+                },
+                {
+                    "case": 234535,
+                    "filing": "2005-11-26T00:00:00.000Z",
+                    "judge": "Abdul Rub Khan",
+                    "order": "evict house immediately and pay 10000"
+                }
+            ]
+        }
+    });
+
+    Template.body.events({
+        "submit .cnic-search": function (event) {
+            // This function is called when the new task form is submitted
+
+            var text = event.target.text.value;
+
+            //User.find({
+            //    cnic: text
+            //});
+
+            Session.set("showResult", 1);
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
